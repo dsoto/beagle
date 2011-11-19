9434ab597ad6476debe369b58edc6828d0eb1a06
Daniel Soto <danielrsoto@gmail.com>
2011-11-19 14:08:19 -0500

    looping with randomization sending to pachube

--- pachube_post.py
+++ pachube_post.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import random
+import time
 
-data_value = 200
-
+# authentication headers 
 headers = {"X-PachubeApiKey": "yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ"}
 
-data={"version":"1.0.0","datastreams":[{"id":"01","current_value":data_value}]}
-
-resp=requests.put('http://api.pachube.com/v2/feeds/39985',headers=headers,data=json.dumps(data))
-
-
+# sleep for a minute, send random number to pachube in json format
+while 1:
+    time.sleep(60)
+    data_value = 100 + random.random() * 50
+    data={"version":"1.0.0","datastreams":[{"id":"01","current_value":data_value}]}
+    resp=requests.put('http://api.pachube.com/v2/feeds/39985',headers=headers,data=json.dumps(data))
