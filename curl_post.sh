d17af1bdc259ce0c9ede8a464eb3e9d5d7ec3844
Daniel Soto <danielrsoto@gmail.com>
2011-11-19 10:01:24 -0500

    this curl command posts to pachube but not when i run the script via ./curl_post.sh

---
+++ curl_post.sh
@@ -0,0 +1,4 @@
+curl --request PUT \
+--data-binary '{"version":"1.0.0","datastreams":[{"id":"01","curent_value":"150"}]}' \
+--header "X-PachubeApiKey: yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ" \
+--verbose http://api.pachube.com/v2/feeds/39985
