167ad811724ee93cef846d619cd4bf641b0adcfc
Daniel Soto <danielrsoto@gmail.com>
2011-11-19 11:52:33 -0500

    substituting variable in data dictionary

--- pachube_post.py
+++ pachube_post.py
@@ -1,9 +1,11 @@
 import requests
 import json
 
+data_value = 200
+
 headers = {"X-PachubeApiKey": "yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ"}
 
-data={"version":"1.0.0","datastreams":[{"id":"01","current_value":"200"}]}
+data={"version":"1.0.0","datastreams":[{"id":"01","current_value":data_value}]}
 
 resp=requests.put('http://api.pachube.com/v2/feeds/39985',headers=headers,data=json.dumps(data))
 
