6ec27c08277e63d220eb97c7155ac6dc6aa82711
Daniel Soto <danielrsoto@gmail.com>
2011-11-19 10:39:38 -0500

    added formatting, still posts to pachube

--- curl_post.sh
+++ curl_post.sh
@@ -1,1 +1,5 @@
-curl --request PUT --data-binary '{"version":"1.0.0","datastreams":[{"id":"01","current_value":"250"}]}' --header "X-PachubeApiKey: yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ" --verbose http://api.pachube.com/v2/feeds/39985
+curl --request PUT \
+--data-binary '{"version":"1.0.0","datastreams":[{"id":"01","current_value":"155"}]}' \
+--header "X-PachubeApiKey: yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ" \
+--verbose \
+http://api.pachube.com/v2/feeds/39985
