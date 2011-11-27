2212d926cc66f90139dddf18b41d9a3f7571314a
Daniel Soto <danielrsoto@gmail.com>
2011-11-26 19:21:12 -0500

    adds functions to post to both pachube and nimbits

--- temp_read_post.py
+++ temp_read_post.py
@@ -13,6 +13,25 @@ tw.log.info('starting pachube_post.py')
 # authentication headers
 headers = {"X-PachubeApiKey": "yKcC6HugqvNtshxI6qEreOPYs9qQG7gZfloc3JQWPbQ"}
 
+def post_nimbits(value):
+    nimits_data = {"email":"drdrsoto@gmail.com",
+               "secret":"01787ade-c6d6-4f9b-8b86-20850af010d9",
+               "point":"603_Test_Stream",
+               "value":value}
+
+    r = requests.post("http://app.nimbits.com/service/currentvalue", data=payload)
+
+def post_pachube(value):
+    data={"version":"1.0.0","datastreams":[{"id":"01","current_value":value}]}
+    try:
+        resp=requests.put('http://api.pachube.com/v2/feeds/39985',headers=headers,data=json.dumps(data))
+    except:
+        tw.log.trace('error').warning('bad request')
+    if resp.status_code == 200:
+        tw.log.info('pachube response value = ' + str(resp.status_code))
+    else:
+        tw.log.error('pachube response value = ' + str(resp.status_code))
+
 # sleep for a minute, send random number to pachube in json format
 while 1:
     tw.log.info("top of loop")
@@ -27,16 +46,11 @@ while 1:
         tw.log.trace('error').warning('bad serial read from arduino')
 
     tw.log.info("arduino temp response = " + str(serial_response))
+
     if serial_response:
         data_value = serial_response / 1024.0 * 5 * 100
 
-    data={"version":"1.0.0","datastreams":[{"id":"01","current_value":data_value}]}
-    try:
-        resp=requests.put('http://api.pachube.com/v2/feeds/39985',headers=headers,data=json.dumps(data))
-    except:
-        tw.log.trace('error').warning('bad request')
-    if resp.status_code == 200:
-        tw.log.info('pachube response value = ' + str(resp.status_code))
-    else:
-        tw.log.error('pachube response value = ' + str(resp.status_code))
+    post_nimbits(data_value)
+    post_pachube(data_value)
+
     time.sleep(60)
