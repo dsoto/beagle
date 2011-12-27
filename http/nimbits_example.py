d96d5193f0ab3d9c0b8b81eea97441b808d77482
Daniel Soto <danielrsoto@gmail.com>
2011-12-27 08:33:37 -0500

    file name change

--- http/nimbits_example.py
+++ http/nimbits_example.py
@@ -6,12 +6,11 @@ while 1:
 
     # generate random data value
     value = random.random() * 10
-    value = 25
 
     # create dictionary
-    data = {"email":"drdrsoto@gmail.com",
-            "secret":"01787ade-c6d6-4f9b-8b86-20850af010d9",
-            "point":"603_Test_Stream",
+    data = {"email":"email@email.com",
+            "secret":"1234567890-1234567890",
+            "point":"My_Test_Stream",
             "value":value}
 
     # use requests library to post to nimbits
