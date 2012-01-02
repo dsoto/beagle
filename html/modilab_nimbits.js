698feafc66b415662721569934373026f2ba6a3a
Daniel Soto <danielrsoto@gmail.com>
2012-01-02 12:05:38 -0500

    adds view of modilab temp via nimbits data

---
+++ html/modilab_nimbits.js
@@ -0,0 +1,139 @@
+// todo - convert these dates to unix epoch milliseconds
+// create date ranges from midnight - midnight local time
+// var now = new Date();
+// var date_end = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+// date_end.setDate(date_end.getDate() + 1);
+// var date_start = new Date(date_end);
+// date_start.setDate(date_start.getDate() - 3);
+
+var now = new Date;
+var date_end = now.getTime();
+var date_start = date_end - 4.0 * 24 * 60 * 60 * 1000;
+
+url =  "http://app.nimbits.com/service/series?"
+       + "email=drdrsoto@gmail.com"
+       + "&secret=01787ade-c6d6-4f9b-8b86-20850af010d9"
+       + "&point=Modi_Lab"
+       // date ranges in unix epoch milliseconds
+       + "&sd=" + date_start
+       + "&ed=" + date_end;
+
+jQuery.getJSON(
+    url,
+    {format:"json"},
+    function (data) {
+        //console.log(data);
+        var times = [],
+            ydata = [];
+        for (i in data) {
+            data[i].d = data[i].d / 4096.0 * 1.8 * 100;
+            //console.log(data[i].d);
+            times[i] = new Date(data[i].timestamp);
+            ydata[i] = data[i].d;
+        }
+        //console.log(data);
+        //console.log(times);
+        //console.log(ydata);
+        var range_round = 1,
+            yrange_max = Math.ceil(d3.max(ydata) / range_round) * range_round,
+            yrange_min = Math.floor(d3.min(ydata) / range_round) * range_round,
+            w = 1000,
+            h = 500,
+            p = 50,
+            x = d3.time.scale.utc()
+                .domain([d3.min(times), d3.max(times)])
+                .range([0, w]),
+            y = d3.scale.linear()
+                .domain([yrange_min, yrange_max])
+                .range([h, 0]);
+
+        // create axes
+        var vis = d3.select("#graph")
+                    .data([data])
+                    .append("svg:svg")
+                    .attr("width", w + p * 2)
+                    .attr("height", h + p * 2)
+                    .append("svg:g")
+                    .attr("transform", "translate(" + p + "," + p + ")");
+
+        // line path
+        vis.append("svg:path")
+            .attr("class", "line")
+            .attr("d", d3.svg.line()
+             // d is some magic that iterates over the d3values object
+                .x(function (d) { return x(d.timestamp); })
+                .y(function (d) { return y(d.d); }));
+
+        // data points as circles
+        vis.selectAll("circle.line")
+            .data(data)
+            .enter().append("svg:circle")
+            .attr("class", "line")
+            .attr("cx", function (d) { return x(d.timestamp); })
+            .attr("cy", function (d) { return y(d.d); })
+            .attr("r", 2);
+
+        var vrules = vis.selectAll("g.vrule")
+                .data(x.ticks(d3.time.hours, 12))
+                .enter().append("svg:g")
+                .attr("class", "rule");
+
+        //vertical grid lines
+        vrules.append("svg:line")
+            //.attr("class", function (d,i) {return i ? null : "axis";})
+            .attr("x1", x)
+            .attr("x2", x)
+            .attr("y1", 0)
+            .attr("y2", h - 1);
+        //x tick text
+        vrules.append("svg:text")
+            .attr("x", x)
+            .attr("y", h + 20)
+            .attr("dy", ".71em")
+            .attr("text-anchor", "middle")
+            //.text(x.tickFormat(d3.time.hours, 2));
+            .text(d3.time.format('%m-%d %H:%M'));
+
+        //vertical axis line
+        //right now this is redundant since a line gets inked for each data element
+        vrules.append("svg:line")
+            // axis has different stroke in css
+            .attr("class", "axis")
+            .attr("x1", x(d3.min(times)))
+            .attr("x2", x(d3.min(times)))
+            .attr("y1", y(yrange_min))
+            .attr("y2", y(yrange_max));
+
+        var hrules = vis.selectAll("g.hrule")
+            .data(y.ticks(10))
+            .enter().append("svg:g")
+            .attr("class", "rule");
+
+        // y tick text
+        hrules.append("svg:text")
+            .attr("y", y)
+            .attr("x", -10)
+            .attr("dy", ".35em")
+            .attr("text-anchor", "end")
+            .text(y.tickFormat(10));
+            //.text(String);
+
+        // horizontal grid lines
+        hrules.append("svg:line")
+            .attr("x1", x(d3.min(times)))
+            .attr("x2", x(d3.max(times)))
+            .attr("y1", y)
+            .attr("y2", y);
+
+        // horizontal axis line
+        hrules.append("svg:line")
+            .attr("class", "axis")
+            .attr("x1", x(d3.min(times)))
+            .attr("x2", x(d3.max(times)))
+            .attr("y1", y(yrange_min))
+            .attr("y2", y(yrange_min));
+
+
+
+});
+
