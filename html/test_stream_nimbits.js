edc6da57330bc165280b8a4df754ae19ac1bd58a
Daniel Soto <danielrsoto@gmail.com>
2012-01-02 12:04:54 -0500

    minor edits

--- html/test_stream_nimbits.js
+++ html/test_stream_nimbits.js
@@ -1,13 +1,15 @@
 var now = new Date;
 var time_end = now.getTime();
-var time_start = time_end - 3.8 * 24 * 60 * 60 * 1000;
+var time_start = time_end - 4.0 * 24 * 60 * 60 * 1000;
 
 url =  "http://app.nimbits.com/service/series?"
        + "email=drdrsoto@gmail.com"
        + "&secret=01787ade-c6d6-4f9b-8b86-20850af010d9"
        + "&point=603_Test_Stream"
+       // date ranges in unix epoch milliseconds
        + "&sd=" + time_start
        + "&ed=" + time_end;
+
 jQuery.getJSON(
     url,
     {format:"json"},
