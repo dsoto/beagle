bb7d56645332d7fd49502d472aa346c049121375
Daniel Soto <danielrsoto@gmail.com>
2012-01-15 15:30:36 -0500

    adds comments

--- gprs/kitchen_bb.py
+++ gprs/kitchen_bb.py
@@ -30,18 +30,19 @@ tw.log.info('---------------------')
 while (1):
     tw.log.info('-- top of loop --')
 
+    # get sensor reading and timestamp
     data_value = bb.read_ain2()
-
     time_stamp = dt.datetime.now().isoformat()
 
     tw.log.info('data_value = ' + str(data_value))
 
+    # post to nimbits over gprs
     bb.initiate_modem_nimbits(s)
     bb.post_nimbits_gprs(data_value, stream_name, s)
     first_response = bb.parse_response(s)
     bb.write_to_db(time_stamp, data_value, first_response, db_cursor, db_connection)
-    bb.post_custom_server_http('kitchen', data_value, time_stamp)
 
+    # turn GPRS off
     s.write('AT#GPRS=0\r\n')
     time.sleep(5)
     response = bb.pause_and_read_serial(s)
@@ -50,6 +51,9 @@ while (1):
     else:
         tw.log.warning('bad GPRS off response')
 
-    # sleep until next minute
+    # post to rackspace over HTTP
+    bb.post_custom_server_http('kitchen', data_value, time_stamp)
+
+    # sleep for a few minutes
     time.sleep(5*60)
 
