27bebdc365adb28d5bf20130ab4013b23ad132fa
Daniel Soto <danielrsoto@gmail.com>
2012-03-20 15:08:08 -0400

    typo

--- gprs/beagle_http_soto.py
+++ gprs/beagle_http_soto.py
@@ -4,7 +4,7 @@ tag = 'kitchen'
 ip = '108.166.92.185'
 
 while (1):
-    data_value = bb.read(ain2())
+    data_value = bb.read_ain2()
     time_stamp = dt.datetime.now().isoformat()
 
     bb.post_custom_server_http(ip, tag, data_value, time_stamp)
