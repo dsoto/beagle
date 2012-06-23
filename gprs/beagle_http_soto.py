5e7832ba183548c978444fe4f0c2b48ae6588d56
Daniel Soto <drdrsoto@gmail.com>
2012-06-23 18:50:45 -0400

    adds high-low logging

--- gprs/beagle_http_soto.py
+++ gprs/beagle_http_soto.py
@@ -5,14 +5,24 @@ import datetime as dt
 tag = 'kitchen'
 ip = '108.166.92.185'
 
+start_temp = bb.read_ain2()
+high = start_temp
+low = start_temp
+
 while (1):
-    data_value = bb.read_ain2()
-    #data_value = 100
-    print data_value
+    data_value = bb.read_ain2(verbose=False)
 
     time_stamp = dt.datetime.now().isoformat()
 
+    if data_value > high:
+        high = data_value
+        print 'all time high', high, time_stamp
+    if data_value < low:
+        low = data_value
+        print 'all time low', low, time_stamp
+
+    # am i doing anything with timestamp?
     bb.post_custom_server_http(ip, tag, data_value, time_stamp)
 
-    #time.sleep(60 * 5)
-    time.sleep(30)
+    time.sleep(60 * 5)
+    #time.sleep(30)
